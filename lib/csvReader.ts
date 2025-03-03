1cb159eb98753669b4b5ce5bb254bfcb35ed1f5f
BK610 <bailey.orion.kane@gmail.com>
2025-03-03 17:58:45 -0500

    Rename to Typescript. Change file to my version that I made for personal-website-next

--- lib/csvReader.ts
+++ lib/csvReader.ts
@@ -2,14 +2,18 @@ import fs from "fs";
 import path from "path";
 import csv from "csv-parser";
 
-export async function readCSV(filePath) {
-  const results = [];
+interface CSVRow {
+  [key: string]: string;
+}
+
+export async function readCSV(filePath: string): Promise<CSVRow[]> {
+  const results: CSVRow[] = [];
   const fullPath = path.join(process.cwd(), filePath);
 
   return new Promise((resolve, reject) => {
     fs.createReadStream(fullPath)
       .pipe(csv())
-      .on("data", (data) => results.push(data))
+      .on("data", (data: CSVRow) => results.push(data))
       .on("end", () => {
         resolve(results);
       })
